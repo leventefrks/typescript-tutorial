f1b1bc047942bed6fecb6d65af25add47d6887ba
x-farkasle <x-farkasle@wizzair.com>
2021-03-07 12:23:41 +0100

    different types - explicit types

--- sandbox.js
+++ sandbox.js
@@ -2,16 +2,37 @@ var names = ['john', 'mary', 'luigi'];
 names.push('julia');
 var numbers = [1, 2, 5, 7, 2];
 numbers.push(11);
-var ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 30
-};
-ninja.age = 66;
+// let ninja = {
+//   name: 'mario',
+//   belt: 'black',
+//   age: 30,
+// };
+// ninja.age = 66;
 // explicit types
 var character;
-var number;
+var age;
 var isLoggedIn;
+age = 66;
+isLoggedIn = true;
 // arrays
+var ninjas = []; // initialize an empty array
+ninjas.push('rambo', 'mickey mouse', 'donkey kong');
+// ninjas.push(66);
 // union types
+var mixed = []; // item can be string or number (several different types)
+mixed.push('jason', 66);
+var uid;
+uid = '1212';
+uid = 454545;
+// but it cannot be boolean
+// uid = false;
 // objects
+var ninja;
+ninja = {
+    name: 'net ninja',
+    age: 66,
+    description: 'old pal'
+};
+ninja = []; // it allowed
+// first you gotta declare the type then the actual values
+var ninjaTwo;
--- sandbox.ts
+++ sandbox.ts
@@ -6,21 +6,60 @@ let numbers = [1, 2, 5, 7, 2];
 
 numbers.push(11);
 
-let ninja = {
-  name: 'mario',
-  belt: 'black',
-  age: 30,
-};
+// let ninja = {
+//   name: 'mario',
+//   belt: 'black',
+//   age: 30,
+// };
 
-ninja.age = 66;
+// ninja.age = 66;
 
 // explicit types
 let character: string;
-let number: number;
+let age: number;
 let isLoggedIn: boolean;
 
+age = 66;
+isLoggedIn = true;
+
 // arrays
+let ninjas: string[] = []; // initialize an empty array
+
+ninjas.push('rambo', 'mickey mouse', 'donkey kong');
+
+// ninjas.push(66);
 
 // union types
 
+let mixed: (string | number | boolean)[] = []; // item can be string or number (several different types)
+
+mixed.push('jason', 66);
+
+let uid: string | number;
+
+uid = '1212';
+uid = 454545;
+
+// but it cannot be boolean
+
+// uid = false;
+
 // objects
+
+let ninja: object;
+
+ninja = {
+  name: 'net ninja',
+  age: 66,
+  description: 'old pal',
+};
+
+ninja = []; // it allowed
+
+// first you gotta declare the type then the actual values
+
+let ninjaTwo: {
+  name: string;
+  age: number;
+  beltColor: string;
+};
