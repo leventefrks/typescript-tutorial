dd2cef4b78119475b2efafa3855368c5d059baa4
x-farkasle <x-farkasle@wizzair.com>
2021-03-07 18:55:08 +0100

    type signatures

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,22 +1,17 @@
 "use strict";
-// let greet: Function;
-// example 1
 var greet;
+// example 1
+var gree;
 greet = function (name, greeting) {
-    console.log(name + " says " + greeting);
+    console.log(name + " says typescript is awesome & sending his " + greeting + "!");
 };
 // example 2
 var calc;
 calc = function (numOne, numTwo, action) {
-    if (action === 'add') {
-        return numOne + numTwo;
-    }
-    else {
-        return numOne - numTwo;
-    }
+    return action === 'add' ? numOne + numTwo : numOne - numTwo;
 };
 // example 3
 var logDetails;
 logDetails = function (ninja) {
-    console.log(ninja.name + " is " + ninja.age + " years old");
+    console.log(ninja.name + " who is " + ninja.age + " thinks typescript is funky!");
 };
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,26 +1,23 @@
-// let greet: Function;
+let greet: Function;
 
 // example 1
-let greet: (a: string, b: string) => void;
+let gree: (a: string, b: string) => void;
 
 greet = (name: string, greeting: string) => {
-  console.log(`${name} says ${greeting}`);
-}
+  console.log(`${name} says typescript is awesome & sending his ${greeting}!`);
+};
 
 // example 2
 let calc: (a: number, b: number, c: string) => number;
 
-calc = (numOne: number, numTwo: number, action: string) => {
-  if (action === 'add') {
-    return numOne + numTwo;
-  } else {
-    return numOne - numTwo;
-  }
-}
+calc = (numOne: number, numTwo: number, action: string) =>
+  action === 'add' ? numOne + numTwo : numOne - numTwo;
 
 // example 3
-let logDetails: (obj: {name: string, age: number}) => void;
+let logDetails: (obj: { name: string; age: number }) => void;
 
-logDetails = (ninja: {name: string, age: number}) => {
-  console.log(`${ninja.name} is ${ninja.age} years old`);
-}
+type person = { name: string; age: number };
+
+logDetails = (ninja: person) => {
+  console.log(`${ninja.name} who is ${ninja.age} thinks typescript is funky!`);
+};
