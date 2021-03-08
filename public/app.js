d05487485f5063cb0f3331a867f40d08d1ee6ea9
x-farkasle <x-farkasle@wizzair.com>
2021-03-08 21:43:31 +0100

    Classes

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 "use strict";
-// classes
+// class
 var Invoice = /** @class */ (function () {
     function Invoice(c, d, a) {
         this.client = c;
@@ -7,25 +7,13 @@ var Invoice = /** @class */ (function () {
         this.amount = a;
     }
     Invoice.prototype.format = function () {
-        return this.client + " owes \u00A3" + this.amount + " for " + this.details;
+        return this.client + " owes $" + this.amount + " for " + this.details + "!";
     };
     return Invoice;
 }());
-var invOne = new Invoice('mario', 'work on the mario website', 250);
-var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+var invoiceOne = new Invoice('john rambo', 'medicine', 400);
+var invoiceTwo = new Invoice('super ninja', 'swords', 100);
+console.log(invoiceOne, invoiceTwo);
 var invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-// invoices.push({ name: 'shaun' });
-console.log(invoices);
-var form = document.querySelector('.new-item-form');
-console.log(form.children);
-// inputs
-var type = document.querySelector('#type');
-var tofrom = document.querySelector('#tofrom');
-var details = document.querySelector('#details');
-var amount = document.querySelector('#amount');
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
-});
+invoices.push(invoiceOne);
+invoices.push(invoiceTwo);
--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,26 @@
-// classes
+// class
 class Invoice {
   client: string;
   details: string;
   amount: number;
 
-  constructor(c: string, d: string, a: number){
+  constructor(c: string, d: string, a: number) {
     this.client = c;
     this.details = d;
     this.amount = a;
   }
 
   format() {
-    return `${this.client} owes £${this.amount} for ${this.details}`;
+    return `${this.client} owes $${this.amount} for ${this.details}!`;
   }
 }
 
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+const invoiceOne = new Invoice('john rambo', 'medicine', 400);
+const invoiceTwo = new Invoice('super ninja', 'swords', 100);
+
+console.log(invoiceOne, invoiceTwo);
 
 let invoices: Invoice[] = [];
-invoices.push(invOne)
-invoices.push(invTwo);
-// invoices.push({ name: 'shaun' });
 
-console.log(invoices);
-
-
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children);
-
-// inputs
-const type = document.querySelector('#type') as HTMLInputElement;
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
-
-form.addEventListener('submit', (e: Event) => {
-  e.preventDefault();
-
-  console.log(
-    type.value, 
-    tofrom.value, 
-    details.value, 
-    amount.valueAsNumber
-  );
-});
+invoices.push(invoiceOne);
+invoices.push(invoiceTwo);
